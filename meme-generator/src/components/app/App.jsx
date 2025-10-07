207607c307eea8a78a6054169da2bbb41dcc8802
Nícolas <nicolasbretschneider1@gmail.com>
2025-10-07 15:30:24 -0300

    cria o quarto projeto

---
+++ meme-generator/src/components/app/App.jsx
@@ -0,0 +1,11 @@
+import Header from '../header/Header.jsx'
+import Main from '../main/Main.jsx'
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Main />
+        </div>
+    )
+}
