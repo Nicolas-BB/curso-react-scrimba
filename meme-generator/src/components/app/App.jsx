56b92a6f3846195dd052bad61150536e14d70b82
Nícolas <nicolasbretschneider1@gmail.com>
2025-10-07 15:34:03 -0300

    adiciona projeto meme-generator

--- meme-generator/src/components/app/App.jsx
+++ meme-generator/src/components/app/App.jsx
@@ -1,9 +1,10 @@
 import Header from '../header/Header.jsx'
 import Main from '../main/Main.jsx'
+import styles from '../../styles/app.module.css'
 
 export default function App() {
     return (
-        <div>
+        <div className={styles.div}>
             <Header />
             <Main />
         </div>
