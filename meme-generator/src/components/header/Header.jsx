19f0b18c711f387548854d16f8a887f1b504e6a4
Nícolas <nicolasbretschneider1@gmail.com>
2025-10-07 16:03:12 -0300

    style: estila o header

--- meme-generator/src/components/header/Header.jsx
+++ meme-generator/src/components/header/Header.jsx
@@ -1,5 +1,11 @@
+import styles from '../../styles/header.module.css'
+import trollface from '../../assets/troll-face.png'
+
 export default function Header() {
     return (
-        <h2>Header</h2>
+        <header className={styles.header}>
+            <img src={trollface} alt="Troll face" />
+            <span>Meme Generator</span>
+        </header>
     )
 }
--- meme-generator/src/components/main/Main.jsx
+++ meme-generator/src/components/main/Main.jsx
@@ -1,5 +1,9 @@
+import styles from '../../styles/main.module.css'
+
 export default function Main() {
     return (
-        <h1>Main</h1>
+        <main>
+
+        </main>
     )
 }
