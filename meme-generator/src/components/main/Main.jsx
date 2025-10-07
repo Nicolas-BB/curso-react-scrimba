e48d774aedf530d232bbbcc5d82780631faaf2a2
Nícolas <nicolasbretschneider1@gmail.com>
2025-10-07 17:52:02 -0300

    feat: adiciona página main

--- meme-generator/src/components/main/Main.jsx
+++ meme-generator/src/components/main/Main.jsx
@@ -2,8 +2,26 @@ import styles from '../../styles/main.module.css'
 
 export default function Main() {
     return (
-        <main>
+        <main className={styles.main}>
+            <form className={styles.form} action={(e) => e.preventDefault}>
+                <div className={styles.inputContainer}>
+                    <label htmlFor="topText" className={styles.textLabel}>
+                        Top text
+                        <input type="text" name="topText" id="bottomText" autoComplete='off' />
+                    </label>
 
+                    <label htmlFor="bottomText" className={styles.textLabel}>
+                        Bottom text
+                        <input type="text" name="bottomText" id="bottomText" />
+                    </label>
+                </div>
+
+                <button>Generate a new meme image 🖼️</button>
+            </form>
+
+            <div className={styles.imgDiv}>
+                Campo de imagem
+            </div>
         </main>
     )
 }
