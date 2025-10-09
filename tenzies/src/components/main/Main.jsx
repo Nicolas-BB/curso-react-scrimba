b9fef0b82938abd9bc67eb8e703e77ca849524ad
Nícolas <nicolasbretschneider1@gmail.com>
2025-10-09 08:49:36 -0300

    setup inicial

---
+++ tenzies/src/components/main/Main.jsx
@@ -0,0 +1,9 @@
+import styles from '../../styles/main.module.css'
+
+export default function Main() {
+    return (
+        <main className={styles.main}>
+            <p>Olá, Mundo</p>
+        </main>
+    )
+}
