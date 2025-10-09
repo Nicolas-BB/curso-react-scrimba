c7a26aacfc067d761cf458244be037a75a1dd5de
Nícolas <nicolasbretschneider1@gmail.com>
2025-10-09 11:05:32 -0300

    feat: estiliza disposição dos Die e adiciona botão ainda sem funcionalidade

--- tenzies/src/components/main/Main.jsx
+++ tenzies/src/components/main/Main.jsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from 'react'
 import styles from '../../styles/main.module.css'
 import Die from '../die/Die.jsx'
 
 export default function Main() {
     return (
         <main className={styles.main}>
-            <Die
-                value='1'
-            />
+            <div className={styles.diceDiv}>
+                <Die value={1} />
+                <Die value={2} />
+                <Die value={3} />
+                <Die value={4} />
+                <Die value={5} />
+                <Die value={6} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+            </div>
+            <button className={styles.button}>Girar dados</button>
         </main>
     )
 }
