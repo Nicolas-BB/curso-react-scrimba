171e345dd91dba882d659136eaab294fb4a14181
Nícolas <nicolasbretschneider1@gmail.com>
2025-10-09 08:55:57 -0300

    style: estiliza a main

--- tenzies/src/components/main/Main.jsx
+++ tenzies/src/components/main/Main.jsx
@@ -3,7 +3,6 @@ import styles from '../../styles/main.module.css'
 export default function Main() {
     return (
         <main className={styles.main}>
-            <p>Olá, Mundo</p>
         </main>
     )
 }
